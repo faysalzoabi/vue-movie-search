13145908902127ef0a514e5fd9361009abf1a87e
Faysal Zoabi <faysal.zoabi@gmail.com>
2019-09-15 16:46:04 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="" class="navbar-brand">Movie Search</a>
+      </nav>
+    </header>
+
+    <main class="container mt-2">
+      <form @submit.prevent = "getResults">
+        <fieldset class="form-group">
+          <label for="searchTerm">Search</label>
+          <input v-model="searchTerm" type="text" class="form-control" id="searchTerm" placeholder="Enter your search">
+        </fieldset>
+        <button type="submit" class="btn btn-primary">GO</button>
+      </form>
+      
+      <section class="row movies-area">
+        <div v-if="error" class="alert alert-danger co">
+          {{error}}
+        </div>
+        <Movie 
+         class="col-4"
+        v-for="movie in results" 
+        :key="movie.imdbID" 
+        :movie="movie"
+        :isInWatchLater="isInWatchLater"
+        :addToWatchLater="addToWatchLater"
+        />
+      </section>
+
+      <section class="mt-2 col-3 row">
+        <h3>Watch Later</h3>
+        <div class="row" id="watch-later">
+          <Movie 
+          class="col-12"
+          v-for="movie in watchLater" 
+          :key="movie.imdbID" 
+          :movie="movie"
+          :removeWatchLater="removeWatchLater"
+            />
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Movie from '@/components/Movie';
+const API_URL = 'http://www.omdbapi.com/?apikey=d54248b1&type=movie&s=';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
+  components:{
+    Movie,
+  },
+  data(){
+    return{
+      error:'',
+      searchTerm:'',
+      results:[],
+      watchLater:[],
+    }
+  },
+  methods:{
+    async getResults(){
+      console.log('hellos')
+      const url = `${API_URL}${this.searchTerm}`;
+      console.log('url', url)
+      const response = await fetch(url);
+      const data = await response.json();
+      if(data.Error) {
+        this.results = [];
+        this.error = data.Error;
+      } else {
+        this.results = data.Search;
+        this.error = '';
+      }
+    },
+    addToWatchLater(movie) {
+      this.watchLater.push(movie);
+    },
+    isInWatchLater(movie) {
+      return this.watchLater.some(wl => wl.imdbID === movie.imdbID);
+    },
+    removeWatchLater(movie) {
+      const index = this.watchLater.indexOf(movie);
+      this.watchLater.splice(index, 1);
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.movies-area{
+  justify-content: space-around;
+  align-items: flex-start;
 }
 </style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card col-4">
+          <img :src="movie.Poster" :alt="movie.Title" class="card-img-top">
+          <div class="card-body">
+            <h4 class="card-title">{{movie.Title}}</h4>
+            <p class="card-text">{{movie.Year}}</p>
+            <button v-if="addToWatchLater && !isInWatchLater(movie)" 
+            @click="addToWatchLater(movie)" 
+            class="btn btn-success">Watch later</button>
+
+            <button v-if="removeWatchLater" 
+            @click="removeWatchLater(movie)" 
+            class="btn btn-danger">Remove</button>
+          </div>
+ </div>
+</template>
+
+<script>
+export default {
+    props:['movie', 'addToWatchLater', 'isInWatchLater', 'removeWatchLater']
+}
+</script>
+
+<style>
+
+</style>
